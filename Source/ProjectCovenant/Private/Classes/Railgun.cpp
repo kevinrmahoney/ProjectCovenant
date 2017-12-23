fb5c2aeeab35e7cb163afd4ebf29fdc2ecb48ae5
Kevin Mahoney <rigelorionbeta@gmail.com>
2017-12-22 19:50:57 -0500

    Renamed folders Added functionality to Railgun to copy that of LightningGun

--- Source/ProjectCovenant/Private/Classes/Railgun.cpp
+++ Source/ProjectCovenant/Private/Classes/Railgun.cpp
@@ -2,7 +2,104 @@
 
 #include "ProjectCovenant.h"
 #include "Railgun.h"
+#include "SubjectZero.h"
 
+// Sets default values
+ARailgun::ARailgun()
+{
+}
 
+// Called when the game starts or when spawned
+void ARailgun::BeginPlay()
+{
+	Super::BeginPlay();
+}
 
+// Called every frame
+void ARailgun::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
 
+	TimeSinceLastShot = TimeSinceLastShot + DeltaTime;
+
+	if(Trigger)
+	{
+		Shoot();
+	}
+}
+
+void ARailgun::SetShooter(ASubjectZero * NewShooter)
+{
+	Shooter = NewShooter;
+}
+
+void ARailgun::SetTrigger(bool T)
+{
+	Trigger = T;
+}
+
+void ARailgun::Shoot()
+{
+	bool DoDamage = false;
+	FVector * StartTrace = new FVector(Muzzle->GetComponentLocation());
+	FVector ForwardVector = Muzzle->GetForwardVector();
+	FVector * EndTrace = new FVector(*StartTrace + (ForwardVector * Range));
+
+	// See if cooldown has passed (while loop prevents shots from being buffered if frame rate is horrendous)
+	while(TimeSinceLastShot >= Cooldown)
+	{
+		TimeSinceLastShot = TimeSinceLastShot - Cooldown;
+		DoDamage = true;
+	}
+
+	if(DoDamage)
+	{
+		FHitResult* HitResult = new FHitResult();
+		FCollisionQueryParams* TraceParams = new FCollisionQueryParams();
+		TraceParams->AddIgnoredActor(Shooter);	// Ignore the Shooter when doing the trace (can't shoot yourself)
+
+												// If firing a round, do a line trace in front of the gun, check if there is a hit, and check if that hit is an actor
+		if(GetWorld()->LineTraceSingleByChannel(*HitResult, *StartTrace, *EndTrace, ECC_Pawn, *TraceParams) && HitResult && HitResult->GetActor())
+		{
+			// Calculate the end of the trace (the actor's hitbox)
+			EndTrace = new FVector(*StartTrace + (HitResult->Distance * ForwardVector));
+
+			// Get the victim and attempt to cast to SubjectZero
+			ASubjectZero * Victim = Cast<ASubjectZero>(HitResult->GetActor());
+			if(Victim && Shooter->HasAuthority())
+			{
+				DealDamage(Victim);
+			}
+		}
+
+		delete HitResult;
+		delete TraceParams;
+	}
+
+	DrawLaser(StartTrace, EndTrace, GetWorld()->DeltaTimeSeconds * 2.f);
+
+	delete StartTrace;
+	delete EndTrace;
+}
+
+void ARailgun::DealDamage(ASubjectZero * Victim)
+{
+	bool Killed = Victim->ReceiveDamage(Damage);
+	Shooter->AddDamageDealt(Damage);
+
+	if(Killed)
+	{
+		Shooter->AddKill();
+		Logger::Log(Shooter->GetPlayerName().ToString() + " has killed " + Victim->GetPlayerName().ToString());
+	}
+}
+
+void ARailgun::DrawLaser(FVector * Begin, FVector * End, float Duration)
+{
+	UWorld * World = GetWorld();
+
+	DrawDebugLine(World, *Begin + FVector(0.2f, 0.f, 0.f), *End, FColor::Red, false, Duration);
+	DrawDebugLine(World, *Begin + FVector(0.f, 0.f, 0.2f), *End, FColor::Red, false, Duration);
+	DrawDebugLine(World, *Begin + FVector(-0.2f, 0.f, 0.f), *End, FColor::Red, false, Duration);
+	DrawDebugLine(World, *Begin + FVector(0.f, 0.f, -0.2f), *End, FColor::Red, false, Duration);
+}
--- Source/ProjectCovenant/Public/Classes/Railgun.h
+++ Source/ProjectCovenant/Public/Classes/Railgun.h
@@ -14,7 +14,60 @@ class PROJECTCOVENANT_API ARailgun : public AHitscanWeapon
 {
 	GENERATED_BODY()
 	
+public:
+	ARailgun();
 	
-	
-	
+protected:
+	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	//float Damage = 5.f;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	//float Range = 20000.f;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	//float Cooldown = 0.1f;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	//float Falloff = 1.f;
+
+	//UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	//float Ammo = 100.f;
+
+private:
+
+	bool Trigger = false;
+	float TimeSinceLastShot = 0.f;
+
+public:
+	//UPROPERTY()
+	//ASubjectZero * Shooter = nullptr;
+
+protected:
+	//UPROPERTY()
+	//USceneComponent * Root;
+
+	//UPROPERTY(VisibleAnywhere)
+	//UStaticMeshComponent * Mesh;
+
+	//UPROPERTY(VisibleAnywhere)
+	//USceneComponent * Muzzle;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void SetTrigger(bool T);
+
+	void SetShooter(ASubjectZero * NewShooter);
+
+private:
+	void Shoot();
+
+	void DealDamage(ASubjectZero * Victim);
+
+	void DrawLaser(FVector * Begin, FVector * End, float Duration);
 };
