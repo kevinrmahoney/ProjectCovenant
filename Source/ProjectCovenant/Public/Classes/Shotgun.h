dc9f3318ac7a46277b2de43e6b3ce7b5e0b6ebdf
Ben Manter <manter.ben@gmail.com>
2018-01-21 00:28:01 +0900

    no message

---
+++ Source/ProjectCovenant/Public/Classes/Shotgun.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Classes/HitscanWeapon.h"
+#include "Shotgun.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTCOVENANT_API AShotgun : public AHitscanWeapon
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
