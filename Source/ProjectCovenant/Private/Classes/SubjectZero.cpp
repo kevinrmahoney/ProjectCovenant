99d2d5cf9ff20538f793682a394c03bb341e9498
Kevin Mahoney <rigelorionbeta@gmail.com>
2018-02-24 08:41:48 -0500

    Added game over music for DM

--- Source/ProjectCovenant/Private/Classes/SubjectZero.cpp
+++ Source/ProjectCovenant/Private/Classes/SubjectZero.cpp
@@ -30,6 +30,8 @@ ASubjectZero::ASubjectZero(const FObjectInitializer& ObjectInitializer)
 
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+
 }
 
 // Called when the game starts or when spawned
