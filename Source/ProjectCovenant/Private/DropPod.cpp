f6ea4072c6de63cb1a80fa3e1a095ba7abf28e62
Kevin Mahoney <rigelorionbeta@gmail.com>
2018-07-09 22:55:28 -0400

    Created Drop Pod mesh, class and blueprint

---
+++ Source/ProjectCovenant/Private/DropPod.cpp
@@ -0,0 +1,54 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectCovenant.h"
+#include "DropPod.h"
+
+
+// Sets default values
+ADropPod::ADropPod()
+{
+	Base = CreateDefaultSubobject<USceneComponent>(TEXT("Base"));
+	SetRootComponent(Base);
+
+	// Mesh
+	DropPodMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DropPod"));
+	DropPodMesh->AttachToComponent(Base, FAttachmentTransformRules::KeepRelativeTransform);
+
+	// Mesh
+	DropPodDoorMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("DropPodDoor"));
+	DropPodDoorMesh->AttachToComponent(DropPodMesh, FAttachmentTransformRules::KeepRelativeTransform);
+
+	// Create a CameraComponent 
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("FirstPersonCamera"));
+	Camera->AttachToComponent(DropPodMesh, FAttachmentTransformRules::KeepRelativeTransform);
+
+	// Position the camera a bit above the eyes
+	Camera->RelativeLocation = FVector(0.f, 0, 250.f);
+	// Allow the pawn to control rotation
+	Camera->bUsePawnControlRotation = true;
+
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ADropPod::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ADropPod::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ADropPod::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
---
+++ Source/ProjectCovenant/Public/DropPod.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "Interactable.h"
+#include "DropPod.generated.h"
+
+UCLASS()
+class PROJECTCOVENANT_API ADropPod : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ADropPod();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USceneComponent * Base;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	UStaticMeshComponent * DropPodMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	UStaticMeshComponent * DropPodDoorMesh;
+
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly)
+	UCameraComponent * Camera;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	
+};
